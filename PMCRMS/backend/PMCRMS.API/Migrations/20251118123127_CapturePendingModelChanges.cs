28cec0c9e345406e388c47a81726093e2d8da6f3
Yash <yash.turmbekar@quasardigital.com>
2025-11-18 18:02:37 +0530

    Update BaseFee for FormConfigurations in migration and model snapshot

---
+++ PMCRMS/backend/PMCRMS.API/Migrations/20251118123127_CapturePendingModelChanges.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMCRMS.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CapturePendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "FormConfigurations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "BaseFee",
+                value: 3000m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "FormConfigurations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "BaseFee",
+                value: 2500m);
+        }
+    }
+}
